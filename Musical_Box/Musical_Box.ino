b4bade749d15d9fc09fee360f087409701b48902
HealthyWaterBottle <anouar.elghazi@dawsoncollege.qc.ca>
2025-05-12 06:35:22 -0400

    Report update & more code comments

--- Musical_Box/Musical_Box.ino
+++ Musical_Box/Musical_Box.ino
@@ -1,7 +1,7 @@
-#include <Keypad.h>   // Library for keypad
-#include "pitches.h"  // File containing the tones
-#include "Mario.h"    // File containing the tones
-#include "Pirates_of_the_Caribbeans.h"
+#include <Keypad.h>                     // Library for keypad
+#include "pitches.h"                    // File containing the tones
+#include "Mario.h"                      // File containing melody A
+#include "Pirates_of_the_Caribbeans.h"  // File containing melody B
 
 #define buzzerPin 11
 /*..................................... Keypad_Setup .............................................*/
@@ -33,28 +33,48 @@ void setup() {
 
 /*....................................... Main logic ............................................*/
 void loop() {
-  char key = customKeypad.getKey();
+  char key = customKeypad.getKey();  // Store the value of the key pressed
 
-  if (key) {
+  if (key) {  // When a key is pressed prints on the serial monitor the key pressed to ease troubleshooting
     Serial.print("Key pressed: ");
     Serial.println(key);
   }
-  if (key == 'A') {
+  if (key == 'A') { // When A is presssed on the keypad, plays melodyA
     melodyA();
   }
-  if (key == 'B') {
+  if (key == 'B') { // When B is presssed on the keypad, plays melodyB
     melodyB();
-    }
-    switch (key) {
-      case '1': tone(buzzerPin, NOTE_A4, 40); break;
-      case '2': tone(buzzerPin, NOTE_B4, 40); break;
-      case '3': tone(buzzerPin, NOTE_C4, 40); break;
-      case '4': tone(buzzerPin, NOTE_D4, 40); break;
-      case '5': tone(buzzerPin, NOTE_E4, 40); break;
-      case '6': tone(buzzerPin, NOTE_F4, 40); break;
-      case '7': tone(buzzerPin, NOTE_G4, 40); break;
-      case '8': tone(buzzerPin, NOTE_A5, 40); break;
-      case '9': tone(buzzerPin, NOTE_B5, 40); break;
-      case '0': tone(buzzerPin, NOTE_C5, 40); break;
-    }
   }
+  switch (key) {
+    case '1':  // When 1 is presssed on the keypad, plays tone A4
+      tone(buzzerPin, NOTE_A4, 40);
+      break;
+    case '2':  // When 2 is presssed on the keypad, plays tone B4
+      tone(buzzerPin, NOTE_B4, 40);
+      break;
+    case '3':  // When 3 is presssed on the keypad, plays tone C4
+      tone(buzzerPin, NOTE_C4, 40);
+      break;
+    case '4':  // When 4 is presssed on the keypad, plays tone D4
+      tone(buzzerPin, NOTE_D4, 40);
+      break;
+    case '5':  // When 5 is presssed on the keypad, plays tone E4
+      tone(buzzerPin, NOTE_E4, 40);
+      break;
+    case '6':  // When 6 is presssed on the keypad, plays tone F4
+      tone(buzzerPin, NOTE_F4, 40);
+      break;
+    case '7':  // When 7 is presssed on the keypad, plays tone G4
+      tone(buzzerPin, NOTE_G4, 40);
+      break;
+    case '8':  // When 8 is presssed on the keypad, plays tone A5
+      tone(buzzerPin, NOTE_A5, 40);
+      break;
+    case '9':  // When 9 is presssed on the keypad, plays tone B5
+      tone(buzzerPin, NOTE_B5, 40);
+      break;
+    case '0':  // When 0 is presssed on the keypad, plays tone C5
+      tone(buzzerPin, NOTE_C5, 40);
+      break;
+  }
+}
--- Musical_Box/Pirates_of_the_Caribbeans.h
+++ Musical_Box/Pirates_of_the_Caribbeans.h
@@ -1,25 +1,16 @@
 #define buzzerPin 11
-
+// different delays fo ease iof use
 int delay1 = 600;
 int delay2 = 200;
 int delay3 = 50;
 
-// to replace the current delays with these I made a notepad file with all the tones and delays
-int d25 = 259;
-int d30 = 311;
-int d35 = 363;
-int d50 = 518;
-int d60 = 622;
-int d75 = 778;
-int d100 = 1037;
-int d125 = 1297;
-
+ 
 void melodyB() {
-  tone(buzzerPin, NOTE_A3, delay2);
-  delay(delay2);
-  noTone(buzzerPin);
-  delay(delay3);
-
+  tone(buzzerPin, NOTE_A3, delay2);  // plays a tone for a given amount of time
+  delay(delay2);                     // delay it isolates the tone
+  noTone(buzzerPin);                 // no tone
+  delay(delay3);                     // delay isolates the tone more
+                                     // repeat for every tone of the melody
   tone(buzzerPin, NOTE_C4, delay2);
   delay(delay2);
   noTone(buzzerPin);
